1775404955fdb875130a0f1877f900f31e2b9e99
cdring <cdring@163.com>
2019-12-24 15:36:47 +0800

    anto

--- src/main/java/com/cdring/jpa/repository/Reader.java
+++ src/main/java/com/cdring/jpa/repository/Reader.java
@@ -16,7 +16,7 @@ import javax.persistence.*;
 public class Reader {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 512)
